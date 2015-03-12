b513a65f81a3e09a28fed0a0742c42d180a7ebae
lexandro <lexandro2000@gmail.com>
2015-03-12 22:58:24 +0100

    Working on training to show units in the right order

--- app/components/training/training.js
+++ app/components/training/training.js
@@ -9,54 +9,67 @@ angular.module('training', ['ngRoute'])
         });
     }])
 
-    .controller('TrainingCtrl', ['$rootScope', '$scope', '$location', 'Country', 'Unit', 'UnitType', function ($rootScope, $scope, $location, Country, Unit, UnitType) {
-        if ($rootScope.token.length < 1) {
-            $location.path('login');
-        } else {
-            var units = Unit.all().query(function () {
-                $rootScope.units = units;
-                $scope.units = $rootScope.units;
-                var unitTypes = UnitType.all().query(function () {
-                    $scope.unitTypes = unitTypes;
-                    $rootScope.unitTypes = unitTypes;
-                    var token = $rootScope.token;
-                    var trainings = Country.trainings(token.token).get({countryId: token.user.countryId}, function () {
-                        $scope.trainings = trainings;
-                        $rootScope.trainings = trainings;
-                        var maxLevel = 6;
-                        $scope.unitTypes.forEach(function (unitType) {
+    .controller('TrainingCtrl', ['$rootScope', '$scope', '$location', 'Country', 'Unit', 'UnitType',
+        function ($rootScope, $scope, $location, Country, Unit, UnitType) {
+            if ($rootScope.token.length < 1) {
+                $location.path('login');
+            } else {
+                var units = Unit.all().query(function () {
+                    $rootScope.units = units;
+                    $scope.units = $rootScope.units;
+                    var unitTypes = UnitType.all().query(function () {
+                        $scope.unitTypes = unitTypes;
+                        $rootScope.unitTypes = unitTypes;
+                        var token = $rootScope.token;
+                        var trainings = Country.trainings(token.token).get({countryId: token.user.countryId}, function () {
+                            $scope.trainings = trainings;
+                            var maxLevel = 1;
+                            var trainingLines = [];
+                            $scope.unitTypes.forEach(function (unitType) {
+                                var trainingLine = {};
+
+                                function createTrainingLine(unitType, unit) {
+                                    var result = {};
+                                    result.unitType = unitType.name;
+                                    result.name = unit.name;
+                                    result.amount = 0;
+                                    result.wounded = 0;
+                                    result.priority = 0;
+                                    trainingLines[trainingLines.length] = result;
+                                    return result;
+                                }
+
                                 if (unitType.id > 0) {
-                                    console.log(unitType);
                                     if (unitType.id < 7) {
                                         // military needs to be reversed and limited age and age-1
                                         $scope.units.slice().reverse().forEach(function (unit) {
                                             if (unit.unitType === unitType.name && (unit.level === maxLevel || unit.level === maxLevel - 1)) {
-                                                console.log("** unit " + JSON.stringify(unit));
+                                                trainingLine = createTrainingLine(unitType, unit);
                                             }
                                         })
                                     } else {
                                         // non-military is in normal order, without lower age restriction
                                         $scope.units.forEach(function (unit) {
                                             if (unit.unitType === unitType.name && unit.level <= maxLevel) {
-                                                console.log("** unit " + JSON.stringify(unit));
+                                                trainingLine = createTrainingLine(unitType, unit);
                                             }
 
                                         })
                                     }
+
                                 }
-                            }
-                        );
+                            });
+                            $scope.trainingLines = trainingLines;
+                        });
                     });
                 });
-            });
 
-
+            }
             $scope.unitFilter = function (unit) {
                 return unit.level < 3;
             }
 
-        }
 
-    }
+        }
     ])
 ;
