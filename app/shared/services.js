3c3edf6510b5478ec4139da9453e1b5306a69b85
lexandro <lexandro2000@gmail.com>
2015-03-21 14:45:01 +0100

    Adding first generic helper service (isEMpty...)

--- app/shared/services.js
+++ app/shared/services.js
@@ -1,6 +1,31 @@
 angular.module('services', [])
+    .factory('Helpers', function () {
 
-    .factory('Authentication', function ($resource, $rootScope) {
+        return {
+            isEmpty: function (obj) {
+                var hasOwnProperty = Object.prototype.hasOwnProperty;
+                // null and undefined are "empty"
+                if (obj == null) return true;
+
+                // Assume if it has a length property with a non-zero value
+                // that that property is correct.
+                if (obj.length > 0)    return false;
+                if (obj.length === 0)  return true;
+
+                // Otherwise, does it have any properties of its own?
+                // Note that this doesn't handle
+                // toString and valueOf enumeration bugs in IE < 9
+                for (var key in obj) {
+                    if (hasOwnProperty.call(obj, key)) return false;
+                }
+
+                return true;
+            }
+        }
+    })
+
+    .
+    factory('Authentication', function ($resource, $rootScope) {
         entry = $resource($rootScope.host + '/authentication/accessTokens?username=:username&password=:password', {});
 
         return entry;
