dfcc8ae102b2d195cfe1a59e608e4fa252d9af71
lexandro <lexandro2000@gmail.com>
2015-03-22 20:18:30 +0100

    Starting attack implementation

--- app/components/attack/attack.js
+++ app/components/attack/attack.js
@@ -23,6 +23,12 @@ angular.module('attack', ['ngRoute'])
                         $rootScope.unitTypes = unitTypes;
                         var token = $rootScope.token;
                         var armies = Country.armies(token.token).get(function () {
+                            var attackArmies = [];
+                            $scope.unitTypes.forEach(function (unitType) {
+                            // http://stackoverflow.com/questions/6298169/how-to-create-a-map-object-in-a-javascript
+                            //    map-es megoldast a dictionarykra
+
+                            });
 
 
                         });
